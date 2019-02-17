c41702c76b7f8d49f3510dec6627acf775b848f6
Mauricio Allende <mauricio.allende.o@gmail.com>
2019-02-17 18:25:19 -0300

    add config

---
+++ src/config.js
@@ -0,0 +1,22 @@
+'use strict'
+
+require('dotenv').config()
+
+const {
+  VPN_USER,
+  VPN_PASS,
+  IMAGE_NAME = 'vpnproxy',
+  DOCKER_PREFIX = 'proxy',
+  PORT = 5050
+} = process.env
+
+module.exports = {
+  user: VPN_USER,
+  pass: VPN_PASS,
+  imageName: IMAGE_NAME,
+  dockerPrefix: DOCKER_PREFIX,
+  proxy: {
+    startsFrom: 5000,
+    port: PORT
+  }
+}
